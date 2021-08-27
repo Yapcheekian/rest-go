ee0bf2591c908ac3972d827e4aa1af2e48369843
Yap Chee Kian <yapcheekian@Yaps-MacBook-Pro.local>
2021-08-27 21:52:53 +0800

    add concurrency to handle multiple requests

--- src/api/domain/repositories/create_repo.go
+++ src/api/domain/repositories/create_repo.go
@@ -1,12 +1,38 @@
 package repositories
 
+import (
+	"strings"
+
+	"github.com/Yapcheekian/rest-go/src/api/utils/errors"
+)
+
 type CreateRepoRequest struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
+}
+
+func (r *CreateRepoRequest) Validate() errors.ApiError {
+	r.Name = strings.TrimSpace(r.Name)
+
+	if r.Name == "" {
+		return errors.NewBadRequestError("invalid repository name")
+	}
+
+	return nil
 }
 
 type CreateRepoResponse struct {
 	Id    int64  `json:"id"`
 	Owner string `json:"owner"`
 	Name  string `json:"name"`
+}
+
+type CreateReposResponse struct {
+	StatusCode int                      `json:"status"`
+	Results    []CreateRepositoryResult `json"results"`
+}
+
+type CreateRepositoryResult struct {
+	Response *CreateRepoResponse `json:"repo"`
+	Error    errors.ApiError     `json:"error"`
 }
--- src/api/services/repositories_service.go
+++ src/api/services/repositories_service.go
@@ -1,7 +1,8 @@
 package services
 
 import (
-	"strings"
+	"net/http"
+	"sync"
 
 	"github.com/Yapcheekian/rest-go/src/api/config"
 	"github.com/Yapcheekian/rest-go/src/api/domain/github"
@@ -17,7 +18,8 @@ var (
 type repoService struct{}
 
 type repoServiceInterface interface {
-	CreateRepo(repositories.CreateRepoRequest) (*repositories.CreateRepoResponse, errors.ApiError)
+	CreateRepo(request repositories.CreateRepoRequest) (*repositories.CreateRepoResponse, errors.ApiError)
+	CreateRepos(requests []repositories.CreateRepoRequest) (repositories.CreateReposResponse, errors.ApiError)
 }
 
 func init() {
@@ -25,10 +27,8 @@ func init() {
 }
 
 func (s *repoService) CreateRepo(input repositories.CreateRepoRequest) (*repositories.CreateRepoResponse, errors.ApiError) {
-	input.Name = strings.TrimSpace(input.Name)
-
-	if input.Name == "" {
-		return nil, errors.NewBadRequestError("invalid repository name")
+	if err := input.Validate(); err != nil {
+		return nil, err
 	}
 
 	request := github.CreateRepoRequest{
@@ -50,4 +50,75 @@ func (s *repoService) CreateRepo(input repositories.CreateRepoRequest) (*reposit
 	}
 
 	return result, nil
+}
+
+func (s *repoService) CreateRepos(requests []repositories.CreateRepoRequest) (repositories.CreateReposResponse, errors.ApiError) {
+	input := make(chan repositories.CreateRepositoryResult)
+	output := make(chan repositories.CreateReposResponse)
+	defer close(output)
+
+	var wg sync.WaitGroup
+
+	go s.handleRepoResults(&wg, input, output)
+
+	for _, request := range requests {
+		wg.Add(1)
+		go s.createRepoConcurrent(request, input)
+	}
+
+	wg.Wait()
+	close(input)
+
+	result := <-output
+
+	successCreations := 0
+	for _, result := range result.Results {
+		if result.Response != nil {
+			successCreations++
+		}
+	}
+
+	if successCreations == 0 {
+		result.StatusCode = result.Results[0].Error.GetStatus()
+	} else if successCreations == len(requests) {
+		result.StatusCode = http.StatusCreated
+	} else {
+		result.StatusCode = http.StatusPartialContent
+	}
+
+	return result, nil
+}
+
+func (s *repoService) handleRepoResults(wg *sync.WaitGroup, input chan repositories.CreateRepositoryResult, output chan repositories.CreateReposResponse) {
+	var results repositories.CreateReposResponse
+
+	for result := range input {
+		results.Results = append(results.Results, result)
+
+		wg.Done()
+	}
+
+	output <- results
+}
+
+func (s *repoService) createRepoConcurrent(input repositories.CreateRepoRequest, output chan repositories.CreateRepositoryResult) {
+	if err := input.Validate(); err != nil {
+		output <- repositories.CreateRepositoryResult{
+			Error: err,
+		}
+		return
+	}
+
+	response, err := s.CreateRepo(input)
+
+	if err != nil {
+		output <- repositories.CreateRepositoryResult{
+			Error: err,
+		}
+		return
+	}
+
+	output <- repositories.CreateRepositoryResult{
+		Response: response,
+	}
 }
